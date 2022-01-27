dbc215515dba6734b0cdd5bd23db86dff51501e2
Samuel Jara <spjara.aravena@gmail.com>
2022-01-27 16:23:15 -0300

    Avance - Version Funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,71 @@
-var resultado = "Inicio"
 var puntajeHumano = 0;
+puntajeHumano = parseInt(puntajeHumano);
 var puntajeIa = 0;
+puntajeIa = parseInt(puntajeIa);
+const cantJuegos = prompt("¿Cuántas veces desea jugar?", "");
+var jugadaHumano;
+var jugadaIa;
+const jugadaRandom = Math.floor(Math.random() * 3) + 1;
+var ganador;
 
-function generarJugadaIa(){
-    const opciones = ("✊", "✋", "✌");
-    let jugadaIa = opciones(Math.floor(Math.random()*opciones.length));
+function genJugadaIa(jugadaRandom, jugadaIa){
+    if (jugadaRandom === 1){
+        jugadaIa = "Piedra";
+    }else if (jugadaRandom === 2){
+        jugadaIa = "Papel";
+    }else if (jugadaRandom === 3){
+        jugadaIa = "Tijera";
+    }
     return jugadaIa;
 }
 
-function calculoResultado(humano, ia){
-    if (humano === ai){
-        resultado = "Empate"
-        return;
-    }else if ((humano === "✊" && ia === "✌") || (humano === "✋" && ia === "✊") || (humano === "✌" && ia === "✋")){
-        puntajeHumano += 1;
-        if (puntajeHumano >= ((resultado /2) + 1)){
-            resultado = "Has Ganado";
-        }else{
-            resultado = "Victoria"
-            return;
-        }
-
-        }else{
-            puntajeIa += 1;
-        }
-        
+function genGanador(jugadaHumano, jugadaIa){
+    if (jugadaHumano === jugadaIa){
+        ganador = "Empate";
+    }else if (jugadaHumano === "Tijera" && jugadaIa === "Papel" || jugadaHumano === "Papel" && jugadaIa === "Piedra" || jugadaHumano === "Piedra" && jugadaIa === "Tijera"){
+        ganador = "Usuario"
+    }else if (jugadaHumano === "Tijera" && jugadaIa === "Piedra" || jugadaHumano === "Papel" && jugadaIa === "Tijera" || jugadaHumano === "Piedra" && jugadaIa === "Papel"){
+        ganador = "Máquina"
     }
+    return ganador;
 }
 
+for (let juego = 1; juego <= cantJuegos; juego++) {
+    jugadaHumano = prompt("Ingresa una de las siguientes opciones: Piedra, Papel o Tijera");
+    jugadaIa = genJugadaIa(jugadaRandom, jugadaIa);
+    ganador = genGanador(jugadaHumano, jugadaIa)
 
-$(function(){
+    if (ganador === "Usuario"){
+        puntajeHumano += 1;
+    }else if (ganador === "Máquina"){
+        puntajeIa += 1;
+    }
 
-    function jugar(jugadaHumano){
-    let jugadaHumano = jugadaHumano.textContent;
-    let jugadaIa = generarJugadaIa();
-    resultado = calculoResultado(jugadaHumano, jugadaIa)
-})
+    if (ganador === "Empate"){
+        $("#resultado").append(`
+            <h3>Juego N°${juego} - Usuario elige: ${jugadaHumano} / Máquina elige: ${jugadaIa} - Empate <br></h3>
+        `);
+    }else{
+        $("#resultado").append(`
+            <h3>Juego N°${juego} - Usuario elige: ${jugadaHumano} / Máquina elige: ${jugadaIa} - Gana: ${ganador} <br></h3>
+        `);
+    }
+
+    $("#puntos-humano").html(puntajeHumano);
+    $("#puntos-ia").html(puntajeIa);
+
+}
+
+if (puntajeHumano > puntajeIa){
+    $("#resultado-final").append(`
+        <h2>¡Felicidades, haz ganado la partida!</h2>
+    `);
+}else if (puntajeHumano === puntajeIa){
+    $("#resultado-final").append(`
+        <h2>La partida ha terminado en empate</h2>
+    `);
+}else{
+    $("#resultado-final").append(`
+        <h2>Haz perdido contra la máquina</h2>
+    `);
+}
